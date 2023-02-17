87a239b2192388b6947820081e3e533cb8795103
gongshang <xinyuzheng31@gmail.com>
2023-02-17 13:39:00 +0800

    酷狗不能用

--- craw_music.py
+++ craw_music.py
@@ -150,7 +150,7 @@ class NetEaseCloud(object):
         try:
             driver.switch_to.frame('g_iframe')
             while True:
-                name_tar = input("\n请输入歌名>>").strip()
+                name_tar = input("\n请输入关键字>>").strip()
                 print("\n正在努力寻找资源.....")
                 if name_tar == 'q':
                     print("\n退出网易云音乐下载")
@@ -181,81 +181,96 @@ class NetEaseCloud(object):
 class KuGou(object):
     def __init__(self):
         pass
+    def download_one_batch(self, data, res, is_index):
+        pattern1 = re.compile(r'(\d+)-(\d+)')
+        pattern2 = re.compile(r'(\d+)')
+        matches1 = pattern1.findall(is_index)
+        matches2 = pattern2.findall(is_index)
+        indexs = []
+        for match1 in matches1:
+            start = match1[0]
+            end = match1[1]
+            indexs.extend(range(int(start), int(end) + 1))
+        for match2 in matches2:
+            start = match2[0]
+            indexs.append(int(start))
+        indexs = sorted(set(indexs))
+        # number = int(input("\n请选择歌曲序号>> "))
+        for number in indexs:
+            if number <= 0:
+                print("请检查输入是否符合规范\n退出酷狗下载")
+                break
+            try:
+                name = str(data[number - 1]['FileName']).replace('<em>', '').replace('</em>', '')
+                fhash = re.findall('"FileHash":"(.*?)"', res)[number - 1]
+                hash_url = "http://www.kugou.com/yy/index.php?r=play/getdata&hash=" + fhash
+                hash_content = requests.get(hash_url)
+                if hash_content == 403:
+                    raise ValueError
+                play_url = ''.join(re.findall('"play_url":"(.*?)"', hash_content.text))
+                real_download_url = play_url.replace("\\", "")
+            except TimeoutError:
+                print("网络不佳，请重新下载")
+                break
+            try:
+                if not os.path.exists("./music/"):
+                    os.makedirs("./music/")
+                save_path = "./music/"+name+".mp3"
+                true_path = os.path.abspath(save_path)
+                print(name + "下载中.....")
+                if os.path.exists(save_path):
+                    continue
+                ct = requests.get(real_download_url).content
+                with open("./music/"+name+".mp3", "wb")as fp:
+                    fp.write(ct)
+                print("{}已保存至{}".format(name, true_path))
+            except:
+                print("无法爬取内容")
+        # 下载成功后退出程序
+        print("下载完毕")
 
     def download(self):
         print("\n", "{:^30}".format("正在使用酷狗VIP下载器"))
         while True:
-            song_name = input("\n请输入歌名>>").strip()
+            song_name = input("\n请输入关键字>>").strip()
             if song_name == '':
                 continue
             elif song_name == 'q':
                 print("\n退出酷狗音乐下载")
                 break
             print("\n正在努力寻找资源......")
-            url = "http://songsearch.kugou.com/song_search_v2?callback=jQuery112407470964083509348_1534929985284&keyword={}&" \
-                  "page=1&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filte" \
-                  "r=0&_=1534929985286".format(song_name)
-            try:
-                res = requests.get(url).text
-                js = json.loads(res[res.index('(') + 1:-2])
-                data = js['data']['lists']
-            except TimeoutError:
-                print("\n网络不佳，请重新下载")
-                break
-            except:
-                print("未找到资源QAQ")
-                break
-            print("为你找到以下内容".center(30, '*'))
-            print("{:6}{:30}".format("序号", "歌手  -  歌名"))
-            try:
-                for i in range(10):
-                    print(str(i + 1) + "    " + str(data[i]['FileName']).replace('<em>', '').replace('</em>', ''))
-                is_index = input("\n选择下载序号(支持多数字输入，以及“-”表示范围，用空格隔开)>>").strip()
-                pattern1 = re.compile(r'(\d+)-(\d+)')
-                pattern2 = re.compile(r'(\d+)')
-                matches1 = pattern1.findall(is_index)
-                matches2 = pattern2.findall(is_index)
-                indexs = []
-                for match1 in matches1:
-                    start = match1[0]
-                    end = match1[1]
-                    indexs.extend(range(int(start), int(end) + 1))
-                for match2 in matches2:
-                    start = match2[0]
-                    indexs.append(int(start))
-                indexs = sorted(set(indexs))
-                # number = int(input("\n请选择歌曲序号>> "))
-                for number in indexs:
-                    if number <= 0:
-                        print("请检查输入是否符合规范\n退出酷狗下载")
-                        break
-                    try:
-                        name = str(data[number - 1]['FileName']).replace('<em>', '').replace('</em>', '')
-                        fhash = re.findall('"FileHash":"(.*?)"', res)[number - 1]
-                        hash_url = "http://www.kugou.com/yy/index.php?r=play/getdata&hash=" + fhash
-                        hash_content = requests.get(hash_url)
-                        play_url = ''.join(re.findall('"play_url":"(.*?)"', hash_content.text))
-                        real_download_url = play_url.replace("\\", "")
-                    except TimeoutError:
-                        print("网络不佳，请重新下载")
-                        break
-                    try:
-                        if not os.path.exists("./music/"):
-                            os.makedirs("./music/")
-                        save_path = "./music/"+name+".mp3"
-                        true_path = os.path.abspath(save_path)
-                        print(name + "下载中.....")
-                        if os.path.exists(save_path):
-                            continue
-                        with open("./music/"+name+".mp3", "wb")as fp:
-                            fp.write(requests.get(real_download_url).content)
-                        print("{}已保存至{}".format(name, true_path))
-                    except:
-                        print("未找到文件夹music\n退出酷狗下载")
-                # 下载成功后退出程序
-                print("下载完毕")
-            except:
-                print("异常错误，请重新下载")
+            page = 1
+            while True:
+                url = "http://songsearch.kugou.com/song_search_v2?callback=jQuery112407470964083509348_1534929985284&keyword={}&" \
+                  "page={}&pagesize=30&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filte" \
+                  "r=0&_=1534929985286".format(song_name, page)
+                try:
+                    res = requests.get(url).text
+                    js = json.loads(res[res.index('(') + 1:-2])
+                    data = js['data']['lists']
+                except TimeoutError:
+                    print("\n网络不佳，请重新下载")
+                    break
+                except:
+                    print("未找到资源QAQ")
+                    break
+                print("为你找到以下内容".center(30, '*'))
+                print("{:6}{:30}".format("序号", "歌手  -  歌名"))
+                try:
+                    for i in range(10):
+                        print(str(i + 1) + "    " + str(data[i]['FileName']).replace('<em>', '').replace('</em>', ''))
+                    print("\n支持多数字输入，以及“-”表示范围，用空格隔开，输入n进入下一页")
+                    while True:
+                        is_index = input("\n选择下载序号>>").strip()
+                        if is_index == "q":
+                            print("已退出，感谢使用！")
+                            return None
+                        if is_index == "n":
+                            page += 1
+                            break
+                        self.download_one_batch(data, res, is_index)
+                except:
+                    print("异常错误，请重新下载")
 
 
 class QQMusic(object):
@@ -298,7 +313,7 @@ class QQMusic(object):
     def qq_music_api(self):
         print("\n", "{:^30}".format("正在使用QQ音乐VIP下载器"))
         while True:
-            song_name = input("\n请输入歌名>>").strip()
+            song_name = input("\n请输入关键字>>").strip()
             print("正在努力寻找资源.........")
             if song_name == '':
                 continue
